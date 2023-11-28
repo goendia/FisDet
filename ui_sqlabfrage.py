44956368798b05adb47fe3fee7a90557d1bfea2c
gitcris <pipapeiler@web.de>
2023-11-28 20:34:47 +0100

    Compile ui files to python files to upgrade to PySide6

--- ui_sqlabfrage.py
+++ ui_sqlabfrage.py
@@ -1,31 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'sqlabfrage.ui'
-#
-# Created: Wed Oct 15 10:27:27 2014
-#      by: pyside-uic 0.2.15 running on PySide 1.2.1
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'sqlabfrage.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from PySide6 import QtCore, QtGui
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QHeaderView, QPushButton,
+    QSizePolicy, QTableView, QVBoxLayout, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
         Dialog.resize(523, 416)
-        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.tableView = QtGui.QTableView(Dialog)
-        self.tableView.setObjectName("tableView")
+        self.verticalLayout = QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.tableView = QTableView(Dialog)
+        self.tableView.setObjectName(u"tableView")
+
         self.verticalLayout.addWidget(self.tableView)
-        self.exportButton = QtGui.QPushButton(Dialog)
-        self.exportButton.setObjectName("exportButton")
+
+        self.exportButton = QPushButton(Dialog)
+        self.exportButton.setObjectName(u"exportButton")
+
         self.verticalLayout.addWidget(self.exportButton)
 
+
         self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "SQL-Abfrage", None, QtGui.QApplication.UnicodeUTF8))
-        self.exportButton.setText(QtGui.QApplication.translate("Dialog", "Als Excel-Datei speichern", None, QtGui.QApplication.UnicodeUTF8))
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"SQL-Abfrage", None))
+        self.exportButton.setText(QCoreApplication.translate("Dialog", u"Als Excel-Datei speichern", None))
+    # retranslateUi
 
--- ui_welcome.py
+++ ui_welcome.py
@@ -1,68 +1,81 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\Manuel\Documents\Fischprojekt\STRUKTURIERT\welcome.ui'
-#
-# Created: Thu Jan 29 15:39:25 2015
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'welcome.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from PySide6 import QtCore, QtGui
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QGroupBox, QLineEdit, QListWidget, QListWidgetItem,
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_Welcome(object):
     def setupUi(self, Welcome):
-        Welcome.setObjectName("Welcome")
+        if not Welcome.objectName():
+            Welcome.setObjectName(u"Welcome")
         Welcome.resize(316, 300)
-        self.buttonBox = QtGui.QDialogButtonBox(Welcome)
-        self.buttonBox.setGeometry(QtCore.QRect(220, 220, 81, 51))
-        self.buttonBox.setOrientation(QtCore.Qt.Vertical)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
-        self.groupBox_7 = QtGui.QGroupBox(Welcome)
-        self.groupBox_7.setGeometry(QtCore.QRect(10, 10, 191, 71))
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
+        self.buttonBox = QDialogButtonBox(Welcome)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setGeometry(QRect(220, 220, 81, 51))
+        self.buttonBox.setOrientation(Qt.Vertical)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.groupBox_7 = QGroupBox(Welcome)
+        self.groupBox_7.setObjectName(u"groupBox_7")
+        self.groupBox_7.setGeometry(QRect(10, 10, 191, 71))
+        sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.groupBox_7.sizePolicy().hasHeightForWidth())
         self.groupBox_7.setSizePolicy(sizePolicy)
-        self.groupBox_7.setMinimumSize(QtCore.QSize(90, 0))
-        self.groupBox_7.setObjectName("groupBox_7")
-        self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox_7)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.editProtokollant_2 = QtGui.QLineEdit(self.groupBox_7)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
+        self.groupBox_7.setMinimumSize(QSize(90, 0))
+        self.verticalLayout_2 = QVBoxLayout(self.groupBox_7)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.editProtokollant_2 = QLineEdit(self.groupBox_7)
+        self.editProtokollant_2.setObjectName(u"editProtokollant_2")
         sizePolicy.setHeightForWidth(self.editProtokollant_2.sizePolicy().hasHeightForWidth())
         self.editProtokollant_2.setSizePolicy(sizePolicy)
-        self.editProtokollant_2.setMinimumSize(QtCore.QSize(10, 0))
-        self.editProtokollant_2.setMaximumSize(QtCore.QSize(16777215, 31))
-        self.editProtokollant_2.setBaseSize(QtCore.QSize(30, 0))
-        font = QtGui.QFont()
-        font.setFamily("Abyssinica SIL")
+        self.editProtokollant_2.setMinimumSize(QSize(10, 0))
+        self.editProtokollant_2.setMaximumSize(QSize(16777215, 31))
+        self.editProtokollant_2.setBaseSize(QSize(30, 0))
+        font = QFont()
+        font.setFamilies([u"Abyssinica SIL"])
         font.setPointSize(14)
         self.editProtokollant_2.setFont(font)
         self.editProtokollant_2.setMaxLength(100)
-        self.editProtokollant_2.setObjectName("editProtokollant_2")
+
         self.verticalLayout_2.addWidget(self.editProtokollant_2)
-        self.listWidget = QtGui.QListWidget(Welcome)
-        self.listWidget.setGeometry(QtCore.QRect(10, 90, 191, 192))
-        self.listWidget.setObjectName("listWidget")
-        self.findDb = QtGui.QPushButton(Welcome)
-        self.findDb.setGeometry(QtCore.QRect(220, 100, 81, 23))
-        self.findDb.setObjectName("findDb")
-        self.createDb = QtGui.QPushButton(Welcome)
-        self.createDb.setGeometry(QtCore.QRect(220, 130, 81, 23))
-        self.createDb.setObjectName("createDb")
+
+        self.listWidget = QListWidget(Welcome)
+        self.listWidget.setObjectName(u"listWidget")
+        self.listWidget.setGeometry(QRect(10, 90, 191, 192))
+        self.findDb = QPushButton(Welcome)
+        self.findDb.setObjectName(u"findDb")
+        self.findDb.setGeometry(QRect(220, 100, 81, 23))
+        self.createDb = QPushButton(Welcome)
+        self.createDb.setObjectName(u"createDb")
+        self.createDb.setGeometry(QRect(220, 130, 81, 23))
 
         self.retranslateUi(Welcome)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Welcome.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Welcome.reject)
-        QtCore.QMetaObject.connectSlotsByName(Welcome)
+        self.buttonBox.accepted.connect(Welcome.accept)
+        self.buttonBox.rejected.connect(Welcome.reject)
+
+        QMetaObject.connectSlotsByName(Welcome)
+    # setupUi
 
     def retranslateUi(self, Welcome):
-        Welcome.setWindowTitle(QtGui.QApplication.translate("Welcome", "Willkommen", None, QtGui.QApplication.UnicodeUTF8))
-        self.groupBox_7.setTitle(QtGui.QApplication.translate("Welcome", "Protokollant", None, QtGui.QApplication.UnicodeUTF8))
-        self.findDb.setText(QtGui.QApplication.translate("Welcome", "find db", None, QtGui.QApplication.UnicodeUTF8))
-        self.createDb.setText(QtGui.QApplication.translate("Welcome", "create db", None, QtGui.QApplication.UnicodeUTF8))
+        Welcome.setWindowTitle(QCoreApplication.translate("Welcome", u"Willkommen", None))
+        self.groupBox_7.setTitle(QCoreApplication.translate("Welcome", u"Protokollant", None))
+        self.findDb.setText(QCoreApplication.translate("Welcome", u"find db", None))
+        self.createDb.setText(QCoreApplication.translate("Welcome", u"create db", None))
+    # retranslateUi
 
